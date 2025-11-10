4339b11262407c37e89a75177ac9f019af9be511
Tagiech <tagiech556@gmail.com>
2025-11-10 21:21:05 +0300

    decompose vk id link

--- VkPhotosExtractor.Web/Controllers/Models/ViewModels/AuthParamsViewModel.cs
+++ VkPhotosExtractor.Web/Controllers/Models/ViewModels/AuthParamsViewModel.cs
@@ -2,5 +2,9 @@ namespace VkPhotosExtractor.Web.Controllers.Models.ViewModels;
 
 public class AuthParamsViewModel
 {
-    public string AuthUri { get; set; } = "";
+    public int VkAppId { get; set; }
+    public string ReturnUri { get; set; } = "";
+    public string State { get; set; } = "";
+    public string CodeChallenge { get; set; } = "";
+    public string AuthRequestUri { get; set; } = "";
 }
